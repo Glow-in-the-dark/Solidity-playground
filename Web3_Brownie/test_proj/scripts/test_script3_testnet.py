587bf5bf5b32ffc216711f828bae7aa9c4ef88ad
Gavin <Gavinlowa111989@gmail.com>
2023-03-31 19:12:06 +0800

    deploying scripts- using brownie (to main/testnet), using .env & config file.
    by using Method 2, u can automate the script, without having to keep keying in the password to the account,whenever it is needed to process transaction or sign.

--- Web3_Brownie/test_proj/scripts/test_script3_testnet.py
+++ Web3_Brownie/test_proj/scripts/test_script3_testnet.py
@@ -1,16 +1,21 @@
 ### This example is for scripting on Mainnet/Testnet  ------
-from brownie import Storage1 , accounts 
+from brownie import Storage1 , accounts, config
 
 def main(): 
     # Accessing the latest deployment instance
-    # the [-1] after the "storage1" stands dor the 
-    contract_instance = Storage1[-1]
-    print(contract_instance)
-    print(Storage1)
+    contract_instance = Storage1[-1] 
+    # If you have deployed multiple instances of the contract, you can access each of them separately by adjusting the index ([-1]).  
 
-
+    # ---- (Method 1) If we load using the accounts, and not via the config file(brownie-config.yaml) ----
     # loading the stored account --- ( run [>> brownie accounts list ] to see what accounts are stored)
-    account = accounts.load("Gavin_test")
+    # account = accounts.load(“Gavin_test”) 
+    # ----------------------------------------------------------------------------------------------------
+
+    # ---- (Method 2) if we lead the account directly using the private key --------------------
+    # (Method 2) is kinda better because you will be asked to confirm your password everytime
+    account = accounts.add(config["account-keys"] ["private-key"])
+    # ------------------------------------------------------------------------------------------
+
     # storing a number
     transaction_receipt = contract_instance.store(15, {"from": account})
     # Wait for transaction confirmation
