254262c990acf8d9e0e2be8fdcbe3576f88feb7b
Gavin <Gavinlowa111989@gmail.com>
2023-04-13 16:04:20 +0800

    Creating "Tasks" on hardhat, and setting up localhost networks. which u can see the transaction logs

--- EtherJS_Hardhat/hardhat-simple-storage/hardhat.config.js
+++ EtherJS_Hardhat/hardhat-simple-storage/hardhat.config.js
@@ -1,10 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -14,6 +17,16 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
     },
+    // This localhost network is different fron the default hardhat network. (it's a separate one)
+    // This is the network, when u run "npx hardhat node", and when u run your script, on --network localhost, u can see the transactions.
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      // accoutns already auto set and given by hardhat
+      chainId: 31337,
+    },
   },
   solidity: "0.8.18",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
--- EtherJS_Hardhat/hardhat-simple-storage/scripts/deploy.js
+++ EtherJS_Hardhat/hardhat-simple-storage/scripts/deploy.js
@@ -1,4 +1,6 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+// use the "run" to run hardhat commands, example verify, rather than doing it manually on the cmd prompt.
+require("dotenv").config();
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
@@ -6,6 +8,43 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract address at: ${simpleStorage.address}`);
+
+  // we only want to RUN the verify func() only if it is on a testnet, and not if it is on Localhost hardhat/ganache testnet.
+  // So check if it is on the sepolia network, && if we have the etherscan API key, before we VERIFY.
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    const wait_blocks = 3;
+    console.log(`Waiting for ${wait_blocks} block txs...`);
+    await simpleStorage.deployTransaction.wait(wait_blocks);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
+
+  //Update the current value
+  const transactionResponse = await simpleStorage.store(8);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`updated value is: ${updatedValue}`);
+}
+
+// verify on etherscan
+// since this contract does not use constructors, we can skip out the "args"
+async function verify(contractAddress, args) {
+  console.log("verifying contract...");
+
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(error);
+    }
+  }
 }
 
 main()
