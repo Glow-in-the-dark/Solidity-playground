63787a202fee5367b824e936f2a580b8ad4f8535
Gavin <Gavinlowa111989@gmail.com>
2023-08-01 00:44:29 +0800

    sorted out the import error somehow.

--- EtherJS_Foundry/foundry-upgrades-proxy/src/BoxV1.sol
+++ EtherJS_Foundry/foundry-upgrades-proxy/src/BoxV1.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-//import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {UUPSUpgradeable} from "../openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+//import {UUPSUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
