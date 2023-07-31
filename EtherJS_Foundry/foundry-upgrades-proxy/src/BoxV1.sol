a1850537405303ad3477fdc2fc39986279f72832
Gavin <Gavinlowa111989@gmail.com>
2023-07-31 22:45:01 +0800

    foundry-upgradable-Proxy

---
+++ EtherJS_Foundry/foundry-upgrades-proxy/src/BoxV1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+//import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "../openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
+    uint256 internal number;
+
+    function getNumber() external view returns(uint256) {
+        return number;
+    }
+
+    function version () external pure returns (uint256){
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {
+        // // anybody can update this, since there is nothing implemented
+
+        // // if u wanna make checks such that only "owner can upgrade this"
+        // if(msg.sender != owner) {
+        //     revert; 
+        // }
+    }
+}
